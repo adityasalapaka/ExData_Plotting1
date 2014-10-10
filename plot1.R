ba4a91a98ec576b1f4b86d03c973aeed3c03703f
adityasalapaka <salapaka.aditya@gmail.com>
2014-10-10 23:13:13 +0530

    Completed code

--- plot1.R
+++ plot1.R
@@ -1,4 +1,3 @@
-rm(list = ls())
 header <- read.table("household_power_consumption.txt", sep = ";", nrows = 1, 
                      header = TRUE)
 data <- read.table("household_power_consumption.txt", sep = ";", 
@@ -8,5 +7,13 @@ rm(header)
 
 datetime <- strptime(paste(data$Date, data$Time, sep = " "), 
                      format = "%d/%m/%Y %H:%M:%S")
-rm(datetime)
+
 data <- data.frame(datetime, data[-c(1,2)])
+
+rm(datetime)
+
+hist(data$Global_active_power, col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot1.png")
+dev.off()
