3e5af2f0f3701517703e71ef92dff9a7e271d814
adityasalapaka <salapaka.aditya@gmail.com>
2014-10-11 02:52:13 +0530

    Added comments

--- plot1.R
+++ plot1.R
@@ -1,16 +1,26 @@
+#Reading headers
+
 header <- read.table("household_power_consumption.txt", sep = ";", nrows = 1, 
                      header = TRUE)
+#Reading relevant data
+
 data <- read.table("household_power_consumption.txt", sep = ";", 
                    na.strings = "?", skip = 66637, nrows = 2880, 
                    col.names = colnames(header))
 rm(header)
 
+#Merging date and time
+
 datetime <- strptime(paste(data$Date, data$Time, sep = " "), 
                      format = "%d/%m/%Y %H:%M:%S")
+
+#Final dataset
 
 data <- data.frame(datetime, data[-c(1,2)])
 
 rm(datetime)
+
+#Creating plot
 
 png("plot1.png")
 hist(data$Global_active_power, col = "red", main = "Global Active Power", 
